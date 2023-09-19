e1b9fe9e65f9d221d53f45d7546d78bf82ec3d7f
Ytalo Santana <142274386+YtaloJacare@users.noreply.github.com>
2023-09-19 16:59:29 -0300

    Add files via upload

---
+++ Exercicio8/Questao1.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+
+int idade, MaiorI, MenorI;
+char Ava;
+
+printf("[Avaliação - Black Cinemas Corporation]");
+
+do{
+    nt++;
+    fflush(stdin);
+
+    do{
+        printf("\nInforme sua Idade:");
+        scanf("%d", &idade);
+        
+        MaiorI > MenorI;
+        if(idade > MenorI && idade == MaiorI){
+            idade =  MaiorI;
+        } else {
+            idade = MenorI;
+        }
+        fflush(stdin);
+
+        if(idade <= 0){
+            printf("\nIdade Invalidade. \n Digite sua Idade:");
+        }
+    }while (idade <= 0);
+
+
+}while(idade != -1);
+}
