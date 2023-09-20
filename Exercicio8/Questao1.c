81a75a9695a2ebf14104693953d59c740338fddf
Ytalo Santana <142274386+YtaloJacare@users.noreply.github.com>
2023-09-19 21:24:19 -0300

    Questao1.c

--- Exercicio8/Questao1.c
+++ Exercicio8/Questao1.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 #include<math.h>
 
+//tem erros
+
 main(){
 
-int idade, MaiorI, MenorI;
+int nt, idade, MaiorI, MenorI, IdadeR, RO, Diffbr, MediaIR;
+float porcentagemR;
 char Ava;
 
-printf("[Avaliação - Black Cinemas Corporation]");
+printf("[Avaliacao - Black Cinemas Corporation]");
 
 do{
     nt++;
@@ -16,19 +19,54 @@ do{
         printf("\nInforme sua Idade:");
         scanf("%d", &idade);
         
-        MaiorI > MenorI;
         if(idade > MenorI && idade == MaiorI){
-            idade =  MaiorI;
+            MaiorI = idade;
         } else {
-            idade = MenorI;
+            MenorI = idade;
         }
         fflush(stdin);
 
         if(idade <= 0){
-            printf("\nIdade Invalidade. \n Digite sua Idade:");
+            printf("\nIdade Invalida!!");
         }
     }while (idade <= 0);
 
+    printf("Sua Opinao em relacao ao filme \n[A - Otimo] \n[B - Bom] \n[C - Regular] \n[D - Ruim] \n[E - Pessimo] \nDigite:");
+    scanf("%c", &Ava);
+
+    switch(Ava){
+
+        case 'a': 
+        Ava = RO;
+        RO++;
+        break;
+
+        case 'b': Ava++;
+        break;
+
+        case 'c': Ava++;
+        break;
+
+        case 'd': Ava++;
+        break;
+
+        case 'e':Ava++;
+        break;
+
+        default: printf("\nNao Existente");
+    }
+
+    do{
+        nt++;
+    } while(nt == Ava);
+
+    printf("Total de Opinioes: %c", nt);
+    printf("Opnioes [Otimos]: %c", RO);
+    
+
+
+    printf("\nDigite [-1] para sair ou [0]:");
+    scanf("%d", &idade);
 
 }while(idade != -1);
 }
