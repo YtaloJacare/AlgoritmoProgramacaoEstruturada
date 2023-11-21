f4c7640c84a7c6ecfe7fcd9488037f09f13552d3
Ytalo Santana <142274386+YtaloJacare@users.noreply.github.com>
2023-11-20 21:01:04 -0300

    Update Questão2.c

--- AtividadeAvaliativa2/Questão2.c
+++ AtividadeAvaliativa2/Questão2.c
@@ -3,33 +3,84 @@
 #include<stdlib.h>
 
 
-//incompleto
-
 typedef struct {
 
-char sexo [500];
+char sexo [20];
+char sexo_extenso[20];
 float salario;
-int id;
 
 } dados;
 
 
 void cadastrar(dados d[], int indice){
 
-    srand(time(0));
-    d[indice].id = rand() % 100;
-    d[indice].status= 1;
+int quantidadeAssalariados;
+
+ for (int i = 0; i < quantidadeAssalariados; i++) {
+        char sexo;
+        float salario;
+        char sexo_extenso[20];
+
+        printf("\nInforme o sexo do assalariado (M para Masculino, F para Feminino): ");
+        scanf(" %c", &sexo);
+
+        printf("Informe o salário do assalariado em reais: ");
+        scanf("%f", &salario);
+
+        if (salario > 1.00){
+            if (sexo == 'M' || sexo == 'm'){
+
+            strcpy(sexo_extenso, "Masculino");
+
+            } else if (sexo == 'F' || sexo == 'f'){
+
+            strcpy(sexo_extenso, "Feminino");
+
+            } else {
+
+            printf("\nSexo inválido.");
+
+                continue;
+            }
+
+            printf("\nSalário: R$ %.2f\n", salario);
+
+            if (salario > salarioMinimo) {
+                printf("Classificação em relação ao salário mínimo: Acima\n");
+            } else if (salario == salarioMinimo) {
+                printf("Classificação em relação ao salário mínimo: Igual\n");
+            } else {
+                printf("Classificação em relação ao salário mínimo: Abaixo\n");
+            }
+
+            printf("Sexo: %s\n", sexo_extenso);
+        } else {
+            printf("Salário inválido.\n");
+        }
+    }
+
 
 
 }
 
+
+void listar(dados d[], int totalPessoas){
+
+for(int i = 0; i < totalPessoas; i++){
+
+printf("\nSexo: %s", d[i].sexo_extenso);
+printf("\nIdade: %d", d[i].salario);
+
+
+}
 
 
 main(){
 
 printf("[-----Cadastro-----]");
 
-int opcaoMenu;
+int opcaoMenu, totalPessoas = 0;
+
 
 do{
 
