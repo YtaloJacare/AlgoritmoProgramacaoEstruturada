6e483695a41730e2919a1fad40e42a8cb4b422bb
Ytalo Santana <142274386+YtaloJacare@users.noreply.github.com>
2023-11-21 15:06:33 -0300

    Questão2.c

--- AtividadeAvaliativa2/Questão2.c
+++ AtividadeAvaliativa2/Questão2.c
@@ -1,111 +1,142 @@
 #include<stdio.h>
 #include<math.h>
+#include<string.h>
 #include<stdlib.h>
 
+#define salarioM 1400.00
 
-typedef struct {
 
-char sexo [20];
+typedef struct{
+
+char sexo[20];
 char sexo_extenso[20];
 float salario;
 
 } dados;
 
+int cadastrar(dados d[], int quantidadeAssalariados){
 
-void cadastrar(dados d[], int indice){
+for(int i = 0; i < quantidadeAssalariados; i++) {
 
-int quantidadeAssalariados;
+    printf("\nInforme o sexo do assalariado (M para Masculino, F para Feminino - Ou digite 'Sair'): ");
+    scanf(" %s", d[i].sexo);
 
- for (int i = 0; i < quantidadeAssalariados; i++) {
-        char sexo;
-        float salario;
-        char sexo_extenso[20];
+    if (strcmp(d[i].sexo, "Sair") == 0 || strcmp(d[i].sexo, "sair") == 0){
 
-        printf("\nInforme o sexo do assalariado (M para Masculino, F para Feminino): ");
-        scanf(" %c", &sexo);
-
-        printf("Informe o salário do assalariado em reais: ");
-        scanf("%f", &salario);
-
-        if (salario > 1.00){
-            if (sexo == 'M' || sexo == 'm'){
-
-            strcpy(sexo_extenso, "Masculino");
-
-            } else if (sexo == 'F' || sexo == 'f'){
-
-            strcpy(sexo_extenso, "Feminino");
-
-            } else {
-
-            printf("\nSexo inválido.");
-
-                continue;
-            }
-
-            printf("\nSalário: R$ %.2f\n", salario);
-
-            if (salario > salarioMinimo) {
-                printf("Classificação em relação ao salário mínimo: Acima\n");
-            } else if (salario == salarioMinimo) {
-                printf("Classificação em relação ao salário mínimo: Igual\n");
-            } else {
-                printf("Classificação em relação ao salário mínimo: Abaixo\n");
-            }
-
-            printf("Sexo: %s\n", sexo_extenso);
-        } else {
-            printf("Salário inválido.\n");
-        }
+        return 1; // Retorna 1 para indicar saída
     }
 
+    printf("Informe o salario em reais: ");
+    scanf("%f", &d[i].salario);
 
+    if(d[i].salario > 1.00){
 
+        if(strcmp(d[i].sexo, "M") == 0 || strcmp(d[i].sexo, "m") == 0){
+
+            strcpy(d[i].sexo_extenso, "Masculino");
+
+        }else if (strcmp(d[i].sexo, "F") == 0 || strcmp(d[i].sexo, "f") == 0) {
+
+            strcpy(d[i].sexo_extenso, "Feminino");
+
+        } else{
+
+            printf("\nSexo invalido.");
+            continue;
+    }
+
+    printf("\nSalario: R$ %.2f\n", d[i].salario);
+
+        if (d[i].salario > salarioM){
+
+        printf("\nRelacao ao salario minimo: Acima");
+
+        } else if (d[i].salario == salarioM) {
+
+            printf("\nRelacao ao salario minimo: Igual");
+
+        } else {
+
+            printf("\nRelacao ao salario minimo: Abaixo");
+
+        }
+
+        printf("\nSexo: %s\n", d[i].sexo_extenso);
+
+    } else {
+
+        printf("\nSalario invalido.\n");
+
+        }
+    }
+    return 0; 
 }
 
+void listar(dados d[], int totalPessoas) {
 
-void listar(dados d[], int totalPessoas){
+    printf("\n[-----Listagem de Dados-----]\n");
 
-for(int i = 0; i < totalPessoas; i++){
+    int cadastrosValidos = 0;
 
-printf("\nSexo: %s", d[i].sexo_extenso);
-printf("\nIdade: %d", d[i].salario);
+    for(int i = 0; i < totalPessoas; i++){
 
+    if (strlen(d[i].sexo) != 0 && d[i].salario > 1.0){
 
+        cadastrosValidos++;
+
+    }
+    
+    }
+
+    if (cadastrosValidos > 0){
+
+        for(int i = 0; i < totalPessoas; i++){
+
+            if(strlen(d[i].sexo) != 0 && d[i].salario > 1.0){
+
+            printf("\nSexo: %s", d[i].sexo_extenso);
+            printf("\nSalario: %.2f\n", d[i].salario);
+
+            }
+        }
+        
+    } else{
+
+        printf("\nNenhum cadastro existente.\n");
+    }
 }
-
 
 main(){
 
-printf("[-----Cadastro-----]");
+    printf("[-----Cadastro-----]\n");
 
-int opcaoMenu, totalPessoas = 0;
+    int opcaoMenu, totalPessoas = 10 ;
+    dados d[totalPessoas];
 
-
-do{
+    do{
 
     printf("\n 1 - Cadastrar");
     printf("\n 2 - Listar");
-    printf("\n 0 - Sair:");
+    printf("\n 0 - Sair\n Sua escolha:");
     scanf("%d", &opcaoMenu);
 
-    switch (opcaoMenu){
+    switch (opcaoMenu) {
 
-    case 1: 
+    case 1:
 
-    cadastrar(d, totalPessoas);
+        if (cadastrar(d, totalPessoas) == 1){
+        printf("\nSaindo do cadastro.\n");
+        break; 
+        }
 
-    break;
+        break;
 
-    case 2: 
-        
-    listar(d, totalPessoas);
+    case 2:
+        listar(d, totalPessoas);
+        break;
+        }
 
-    break;
+    } while(opcaoMenu != 0);
 
-    }
-
-} while(opcaoMenu != 0);
-
-
+    return 0;
 }
