badfd9547e905e7a5099b29b61701cd361adf9d7
Ytalo Santana <142274386+YtaloJacare@users.noreply.github.com>
2023-12-06 17:14:42 -0300

    Add files via upload

---
+++ Calculo/calculo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+main(){
+
+int num1 = 60, num2 = 20, soma, sub, div, mult;
+
+soma = num1 + num2;
+sub = num1 - num2;
+div = num1 / num2;
+mult = num1 * num2;
+
+printf("Valores; 10 e 2");
+printf("\n A soma: %d", soma);
+printf("\n A subtracao: %d", sub);
+printf("\n A dvisao: %d", div);
+printf("\n A multiplicacao: %d", mult);
+
+}
